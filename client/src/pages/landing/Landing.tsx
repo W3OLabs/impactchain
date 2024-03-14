9cebd700c30379c6a0e21de31709fe5ed3c7a771
aschouw30 <217284183@mycput.ac.za>
2024-03-14 13:02:47 +0200

    aneeqah landing

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -2,20 +2,23 @@ import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div className="w-[auto] h-[800px] m-0 min-h-100vh relative bg-gradient-to-t from-black to-green-500 overflow-hidden flex-col">
-      <img className="w-[330px] h-[330px] items-center top-[30px] left-[450px] absolute" src="/green-icon.svg" alt="impact chain logo"/>
-      <div className="w-[793px] left-[223px] top-[300px] absolute text-center">
-        <span className="text-white text-[80px] font-extrabold font-NeueMachinaUltrabold">Welcome to </span>
-        <span className="text-green-400 text-[80px] font-extrabold font-NeueMachinaUltrabold">impact.</span>
-        <span className="text-white text-[80px] font-extrabold font-NeueMachinaUltrabold">chain</span>
+    <div className=" min-h-screen bg-gradient-to-t from-black to-green-500 overflow-hidden flex-col flex items-center">
+      <img className="w-[330px] h-[330px]" src="/green-icon.svg" alt="impact chain logo"/>
+      <div className="flex flex-col items-center justify-center md:text-7xl text-5xl">
+        <h1 className="text-white  font-extrabold font-NeueMachinaUltrabold">Welcome to </h1>
+        <h1>
+        <span className="text-green-400  font-extrabold font-NeueMachinaUltrabold">impact.</span>
+        <span className="text-white  font-extrabold font-NeueMachinaUltrabold">chain</span>
+        </h1>
       </div>
-      <div className="relative w-[240px] h-[40px] top-[600px] mr-0 mb-100px left-[500px] bg-[#4fef64] z-3 rounded-[20px]">
-        <Link to="login">
-          <span className="w-[156px] h-[33px] left-[42px] top-[4px] absolute text-center text-black text-xl font-extrabold font-['PP Telegraf']">
-            Login
-          </span>
+      
+        <Link to="login" 
+        className=" mt-20 bg-custom-green px-20 py-2 items-center font-['PP Telegraf'] font-extrabold text-xl text-center rounded-full"
+        >
+          Login
+          
         </Link>
-      </div>
+      
       
     </div>
   );
