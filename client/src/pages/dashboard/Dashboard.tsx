91d69c1c2c85774461b9b2027c28c4aa3a6d0cc7
aschouw30 <217284183@mycput.ac.za>
2024-03-26 17:46:27 +0200

    fontface on dashboard

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+    <div className="w-[393px] h-[50px] text-center text-white text-[40px] font-extrabold fontfamily-['Neue Machina']"> 
+    Impact Overview
+    </div>
+    <div></div>
+    </div>
   )
 }
 
